eb8dcb93ac0676ec497428e8f4e26f1c99e764f5
dumpalareddy93 <reddydumplala93@gmail.com>
2025-09-25 13:48:10 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0d1e1b3241c4a66bd" # replace with your SG ID
-
+SG_ID="sg-07c8acf3fa6b923fa" # replace with your SG ID
+ZONE_ID="Z10225341OKKJ5ODTVYKC" # replace with your ID
+DOMAIN_NAME="daws86.space"
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-id $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86.space
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        
-        echo "$instance: $IP"
+        RECORD_NAME="$DOMAIN_NAME" # daws86.space
+    fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
