802de0fe7a34d159620b747151c5f247ce05d7ac
dumpalareddy93 <reddydumplala93@gmail.com>
2025-09-25 13:40:41 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ SG_ID="sg-0d1e1b3241c4a66bd" # replace with your SG ID
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0d1e1b3241c4a66bd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-id $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
@@ -15,6 +15,5 @@ do
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         
-
-    echo "$instance: $IP"
-   done
+        echo "$instance: $IP"
+done
